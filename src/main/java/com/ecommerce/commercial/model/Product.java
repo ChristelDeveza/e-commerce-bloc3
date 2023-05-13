7ebf5e32951c5436b731afcb8880b4aa4ae95e13
ChristelDeveza <chris.dvz66@gmail.com>
2023-05-13 09:07:48 +0200

    category add in product

--- src/main/java/com/ecommerce/commercial/model/Product.java
+++ src/main/java/com/ecommerce/commercial/model/Product.java
@@ -1,10 +1,13 @@
 package com.ecommerce.commercial.model;
 
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 // import jakarta.persistence.Lob;
 import jakarta.persistence.SequenceGenerator;
 
@@ -38,16 +41,21 @@ public class Product {
   //   this.image = image;
   // }
 
-  public Product(String name, Long price) {
+  @OneToMany(mappedBy = "product")
+  private List<Category> category;
+
+  public Product(String name, Long price, List<Category> category) {
     this.name = name;
     this.price = price;
+    this.category = category;
     
   }
 
-  public Product(Long id, String name, Long price) {
+  public Product(Long id, String name, Long price, List<Category> category) {
     this.id = id;
     this.name = name;
     this.price = price;
+    this.category = category;
    
   }
   public Product() {
@@ -76,6 +84,14 @@ public class Product {
 
   public void setPrice(Long price) {
     this.price = price;
+  }
+
+  public List<Category> getCategory() {
+    return category;
+  }
+
+  public void setCapacitesList(List<Category> category) {
+    this.category = category;
   }
 
 //   public byte[] getImage() {
