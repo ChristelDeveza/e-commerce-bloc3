2fab210afb12c75f1c50e978513ae1e7e931ad9e
ChristelDeveza <chris.dvz66@gmail.com>
2023-05-21 19:54:44 +0200

    put request product with connexion user

--- src/main/java/com/ecommerce/commercial/controller/UpdateProductController.java
+++ src/main/java/com/ecommerce/commercial/controller/UpdateProductController.java
@@ -1,6 +1,8 @@
 package com.ecommerce.commercial.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -8,8 +10,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ecommerce.commercial.config.JwtConfig;
 import com.ecommerce.commercial.model.UpdateProduct;
 import com.ecommerce.commercial.service.UpdateProductService;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 @CrossOrigin
@@ -21,7 +26,18 @@ private UpdateProductService updateProductService;
 
   
 @PutMapping("{id}")
-public void updateProduct(@PathVariable(name="id") Long id, @RequestBody UpdateProduct updateProduct) {
+public ResponseEntity<String> updateProduct(@PathVariable(name="id") Long id, @RequestBody UpdateProduct updateProduct, HttpServletRequest request) {
+  String jwt = request.getHeader("Authorization");
+  String newJwt = jwt.replace("Bearer ", "");
+  System.out.print(newJwt);
+  if (newJwt == null || newJwt.isEmpty()) {
+      return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("JWT manquant");
+  }
+
+  if (JwtConfig.validateJwt(newJwt)) {
+      return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("JWT invalide");
+  }
   updateProductService.updateProduct(id, updateProduct);
+  return ResponseEntity.ok("Produit mis à jour avec succès");
 }
 }
